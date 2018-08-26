9c2e1b512f9f712568e4f007db7ddda3ec8f33f8
Raja <raja.aeccrj@gmail.com>
2018-08-26 22:27:36 +0530

    First Commit

---
+++ SeleniumTest/src/main/java/com/dutta/seleniumTest/App.java
@@ -0,0 +1,43 @@
+package com.dutta.seleniumTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main( String[] args ) throws Exception
+    {
+    	System.setProperty("webdriver.chrome.driver","E:\\chromedriver_win32\\chromedriver.exe");
+    	ChromeOptions chromeOptions = new ChromeOptions();
+    	WebDriver driver = new ChromeDriver(chromeOptions);
+
+    	System.out.println("Hi, Welcome to Edureka's YouTube Live session on Selenium WebDriver");
+    	
+    	driver.get("http://localhost:8081/selenium-website");
+    	driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+    	driver.findElement(By.name("userName")).sendKeys("dutta");
+    	//Thread.sleep(5000); 
+    	driver.findElement(By.name("pass")).sendKeys("dutta");
+    	//Thread.sleep(5000); 
+    	driver.findElement(By.id("submit-3")).click();
+        //Thread.sleep(5000); 	
+    	String title = driver.getTitle();
+    	if(title.equalsIgnoreCase("Welcome Page")){
+    		System.out.println("Login Successful");
+    	}
+    	else{
+    		System.out.println("Login Failed");
+    	}
+        driver.quit();
+    	
+
+
+    }
+}
