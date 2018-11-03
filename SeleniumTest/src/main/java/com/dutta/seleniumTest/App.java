d18af0ce181772ad327f0f2d728bb9fccd6175af
Raja <raja.aeccrj@gmail.com>
2018-11-03 23:31:43 +0530

    Files updated

--- SeleniumTest/src/main/java/com/dutta/seleniumTest/App.java
+++ SeleniumTest/src/main/java/com/dutta/seleniumTest/App.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -12,36 +13,36 @@ import org.openqa.selenium.chrome.ChromeOptions;
  *
  */
 public class App {
-	public static void main( String[] args ) throws Exception
-    {
-    	//System.setProperty("webdriver.chrome.driver","E:\\chromedriver_win32\\chromedriver.exe");
-    	System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-    	ChromeOptions chromeOptions = new ChromeOptions();
-    	chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--no-sandbox");
-    	WebDriver driver = new ChromeDriver(chromeOptions);
+	public static void main(String[] args) throws Exception {
+//		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		chromeOptions.addArguments("--no-sandbox");
+		WebDriver driver = new ChromeDriver(chromeOptions);
 
-    	System.out.println("Hi, Welcome to Edureka's YouTube Live session on Selenium WebDriver");
-    	
-    	//driver.get("http://localhost:8081/selenium-website");
-    	driver.get("http://18.223.124.18:8181/selenium-website");
-    	driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
-    	driver.findElement(By.name("userName")).sendKeys("dutta");
-    	//Thread.sleep(5000); 
-    	driver.findElement(By.name("pass")).sendKeys("dutta");
-    	//Thread.sleep(5000); 
-    	driver.findElement(By.id("submit-3")).click();
-        //Thread.sleep(5000); 	
-    	String title = driver.getTitle();
-    	if(title.equalsIgnoreCase("Welcome Page")){
-    		System.out.println("Login Successful");
-    	}
-    	else{
-    		System.out.println("Login Failed");
-    	}
-        driver.quit();
-    	
+		System.out.println("Hi, Welcome Edureka Certification Project");
 
+		driver.get("http://18.222.157.160:8091/index.php");
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.findElement(By.id("About Us")).click();
+//		Thread.sleep(5000);
+		WebElement element = driver.findElement(By.tagName("b"));
+		String text = element.getText();
+		System.out.println("text=" + text);
 
-    }
+		if (text.equalsIgnoreCase("about")) {
+			System.out.println("Certification Project test Successful");
+		} else {
+			System.out.println("Certification Project test Failed");
+		}
+		// driver.findElement(By.id("PID-ab2-pg"))args;
+		// if(title.equalsIgnoreCase("Welcome Page")){
+		// System.out.println("Login Successful");
+		// }
+		// else{
+		// System.out.println("Login Failed");
+		// }
+		driver.quit();
+	}
 }
